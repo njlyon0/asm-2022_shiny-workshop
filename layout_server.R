1a3d243f82651c9ff3e2ec3fb19adf1109e8678f
njlyon0 <nicholasjlyon@gmail.com>
2022-06-10 13:40:00 -0500

    Dropped redundant reactive call of inputs and just used the inputs directly in plot assembly

--- layout_server.R
+++ layout_server.R
@@ -2,23 +2,16 @@
 # Assemble server function
 layout_server <- function(input, output){
 
-  # Reactively grab the user's inputs
-  slider_val <- reactive({ input$bin_num })
-  x_name <- reactive({ input$x_lab })
-  y_name <- reactive({ input$y_lab })
-  plot_main <- reactive({ input$title })
-  color_choice <- reactive({ input$color })
-
   # Histogram assembly
   output$histo <- renderPlot({
     graphics::hist(x = sample(x = 1:1000,
                               size = 100,
                               replace = TRUE),
-                   breaks = slider_val(),
-                   col = color_choice(),
-                   xlab = x_name(),
-                   ylab = y_name(),
-                   main = plot_main())
+                   breaks = input$bin_num,
+                   col = input$color,
+                   xlab = input$x_lab,
+                   ylab = input$y_lab,
+                   main = input$title)
   })
 
 } # Close `server{...`
